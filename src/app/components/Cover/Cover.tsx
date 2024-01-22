a5dc4cfb3d549c7f5156552a490e343e22383751
Lucas Fonseca <lucasfonsecaeditor@gmail.com>
2024-01-21 23:45:58 -0300

    feat: create cover

---
+++ src/app/components/Cover/Cover.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Cover: React.FC = () => {
+  return (
+    <section className="w-full h-dvh bg-primary-900">
+      <div className="w-full max-w-[1170px] h-full mx-auto text-white"></div>
+    </section>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Cover } from './components/Cover';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Blue Team Tech</h1>
+    <main>
+      <Cover />
     </main>
   );
 }
