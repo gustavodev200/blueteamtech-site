63502fbe56f59a5021ebfb5ea8a25e40cdfa8175
gustavodev200 <gustavolage200@gmail.com>
2024-01-28 23:30:44 -0300

    fix: image logo

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -59,7 +59,7 @@ export const NavBar: React.FC = () => {
       </div>
 
       <img
-        src="/logo.svg"
+        src="/images/logo.svg"
         alt="Logo"
         width={114}
         height={50}
